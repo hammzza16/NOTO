4800c070e121ebf7ecd25c532a8bddeef50b96cd
hammzza16 <hamzashaikh.hs34@gmail.com>
2025-04-08 16:19:18 +0530

    password input box

--- frontend/NOTO-APP/src/pages/Login/Login.jsx
+++ frontend/NOTO-APP/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import {Link} from "react-router-dom"
+import PasswordInput from '../../components/Input/PasswordInput'
 
 
 const Login = () => {
@@ -11,8 +12,8 @@ const Login = () => {
         <form onSubmit={()=>{}}>
           <h4 className='text-3xl mb-7'>Login</h4>
           <input type='text' placeholder='Email' className='input-box'/>
+          <PasswordInput/>
           <button type='submit' className='btn-primary'>Login</button>
-
           <p className='text-sm text-center mt-4' >
             Not Registered yet? <Link to="/signup" className="font-medium text-primary underline">Ceate an account</Link>
           </p>
