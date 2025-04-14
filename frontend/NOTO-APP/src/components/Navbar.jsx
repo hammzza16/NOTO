eb24a3046ae6b408686a6f067878e16249184b72
hammzza16 <hamzashaikh.hs34@gmail.com>
2025-04-14 16:36:13 +0530

    SeacrBar INPUT

--- frontend/NOTO-APP/src/components/Navbar.jsx
+++ frontend/NOTO-APP/src/components/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileInfo from "./Cards/profileInfo";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Router,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+import SearchBar from "./SearchBar/SearchBar";
 
 const Navbar = () => {
+  let [searchQuery, setSearchQuery] = useState("");
+  let navigate = useNavigate();
+
+  let onLogout = () => {
+    navigate("/login");
+  };
+
+  let handleSearch = () => {};
+
+  let onClearSearch = () => {
+    setSearchQuery("");
+  };
+
   return (
     <div className="flex items-center justify-between bg-white px-6 py-2 drop-shadow">
       <h2 className="text-2xl font-medium text-black py-2">NOTO</h2>
-      <ProfileInfo />
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
